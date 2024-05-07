7af194835636dad4485d7fd0659faf63c7d4b32d
RishanRashid <rishanrashi@gmail.com>
2024-05-07 12:06:50 +0530

    Initial Commit

---
+++ universityList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  universityList
+//
+//  Created by Allnet Systems on 5/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
